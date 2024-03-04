a02c68b1e635d2938aa724f0ab661a35434a0d09
Prashanth Jayaram <powershellsql@gmail.com>
2024-03-04 00:24:59 -0500

    Update LoadingAzurePowerShellmodules.ps1

--- Ch10/LoadingAzurePowerShellmodules.ps1
+++ Ch10/LoadingAzurePowerShellmodules.ps1
@@ -2,3 +2,8 @@
 PS C:\> Install-Module Az
 PS C:\> Import-Module Az
 PS C:\> Get-Module Azure | Select Version
+## List Azure Modules
+PS C:\> Get-Module -ListAvailable -Name Az*
+
+## count of cmdlets
+PS C:\Users\pjayaram> (Get-Command -Module Az.compute -Type Cmdlet).count

70bf998d6b1cb2fc6de3a523c99ee173a594a09c
Prashanth Jayaram <powershellsql@gmail.com>
2024-03-04 00:26:33 -0500

    Create CreateAzureAutomationAccount.ps1

---
+++ Ch10/CreateAzureAutomationAccount.ps1
@@ -0,0 +1,3 @@
+To create a new Azure Automation account, use the New-AzAutomationAccount cmdlet in PowerShell. The command breakdown is:
+
+New-AzAutomationAccount -Name "myazureautomationacct" -Location "central India" -ResourceGroupName "MyPowerShellRG"

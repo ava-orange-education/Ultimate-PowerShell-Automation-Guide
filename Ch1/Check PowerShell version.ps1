018d7236176f77114b4cf37744e4a29a88ae9724
rajendragp <42903376+rajendragp@users.noreply.github.com>
2024-05-09 09:16:26 +0530

    Update Check PowerShell version.ps1

--- Ch1/Check PowerShell version.ps1
+++ Ch1/Check PowerShell version.ps1
@@ -2,3 +2,13 @@
 $psVersion = $PSVersionTable.PSVersion
 # Display version information
 Write-Host "PowerShell Version: $($psVersion.Major).$($psVersion.Minor).$($psVersion.Build).$($psVersion.Revision)"
+
+
+# Minimum required version
+$minRequiredVersion = [Version]'6.0.0'
+# Check if the installed version meets the requirement
+if ($PSVersionTable.PSVersion -ge $minRequiredVersion) {
+    Write-Host "PowerShell version is sufficient to run the script."
+} else {
+    Write-Host "PowerShell version does not meet the minimum requirement. Please upgrade PS version."
+}

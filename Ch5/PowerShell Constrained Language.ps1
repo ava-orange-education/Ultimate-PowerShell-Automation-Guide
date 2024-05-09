3b4f8055d0918b02976d95ef9155fa394c1b5398
rajendragp <42903376+rajendragp@users.noreply.github.com>
2024-05-09 10:07:56 +0530

    Create PowerShell Constrained Language.ps1

---
+++ Ch5/PowerShell Constrained Language.ps1
@@ -0,0 +1,6 @@
+$ExecutionContext.SessionState.LanguageMode 
+# Output: FullLanguage 
+$ExecutionContext.SessionState.LanguageMode = "ConstrainedLanguage" $ExecutionContext.SessionState.LanguageMode 
+# Output: ConstrainedLanguage 
+
+

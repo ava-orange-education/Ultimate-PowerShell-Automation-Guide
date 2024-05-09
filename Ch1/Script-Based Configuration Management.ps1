b90e461b0deb8f1a885ca3b3563923cdfd7519e0
rajendragp <42903376+rajendragp@users.noreply.github.com>
2024-05-09 09:13:20 +0530

    Create Script-Based Configuration Management.ps1

---
+++ Ch1/Script-Based Configuration Management.ps1
@@ -0,0 +1,21 @@
+$XML='C:\Ultimate PowerShell Automation\webconfig.XML'
+Get-Content $XML
+
+# Define parameters for the script
+PARAM(
+    [String]$XML='C:\Ultimate PowerShell Automation\webconfig.XML',
+    [String]$DatabaseName='TargetDB',
+    [String]$DatabaseServer='DataCenterDB01'
+)
+
+# Create strings to be used for replacement
+[string]$db = 'Catalog='+ $DatabaseName
+[string]$dbs = 'Source='+ $DatabaseServer
+
+# Read the content of the XML file
+(Get-Content $XML) | 
+Foreach-Object {
+    # Replace "Catalog=..." with the specified database name
+    # Replace "Source=..." with the specified database server
+    $_ -replace "Catalog=([A-Za-z0-9_]+)", $db -replace "Source=([A-Za-z0-9_]+)", $dbs
+} | Set-Content $XML
